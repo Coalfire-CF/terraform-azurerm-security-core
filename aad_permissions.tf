f2dd6227dd91e6b704f2c3787727637174734aa6
Hunter Capestany <Hunter.Capestany@coalfire.com>
2023-02-28 09:45:08 -0500

    Test Conditional For_each

--- aad_permissions.tf
+++ aad_permissions.tf
@@ -33,14 +33,14 @@ resource "azurerm_role_assignment" "assign_sub_user_access" {
 }
 
 resource "azurerm_role_assignment" "assign_app_sub_contributor" {
-  for_each             = { for entry in local.app_sub_user_mapping : "${substr(entry.user, -12, -1)}_${substr(entry.subscription_id, -12, -1)}" => entry }
+  for_each             = { for entry in local.app_sub_user_mapping : "${substr(entry.user, -12, -1)}_${substr(entry.subscription_id, -12, -1)}" => entry if var.enable_aad_permissions }
   scope                = "/subscriptions/${each.value.subscription_id}"
   role_definition_name = "Contributor"
   principal_id         = each.value.user
 }
 
 resource "azurerm_role_assignment" "assign_app_sub_user_access" {
-  for_each             = { for entry in local.app_sub_user_mapping : "${substr(entry.user, -12, -1)}_${substr(entry.subscription_id, -12, -1)}" => entry }
+  for_each             = { for entry in local.app_sub_user_mapping : "${substr(entry.user, -12, -1)}_${substr(entry.subscription_id, -12, -1)}" => entry if var.enable_aad_permissions }
   scope                = "/subscriptions/${each.value.subscription_id}"
   role_definition_name = "User Access Administrator"
   principal_id         = each.value.user
