f76ee0d7f9b12b69da1d141d36c668bc9d9b148f
Douglas Francis <douglas@poshops.io>
2023-02-28 08:25:35 -0600

    cleanup

--- aad_permissions.tf
+++ aad_permissions.tf
@@ -40,24 +40,8 @@ resource "azurerm_role_assignment" "assign_app_sub_contributor" {
 }
 
 resource "azurerm_role_assignment" "assign_app_sub_user_access" {
-  for_each = var.enable_aad_permissions ? [] ? tomap({ local.app_sub_user_mapping : "${substr(local.app_sub_user_mapping.user, -12, -1)}_${substr(local.app_sub_user_mapping.subscription_id, -12, -1)}" }) : {}
-
-
-  #for_each             = { for entry in local.app_sub_user_mapping : "${substr(entry.user, -12, -1)}_${substr(entry.subscription_id, -12, -1)}" => entry }
+  for_each             = { for enty in local.app_sub_user_mapping : "${substr(local.app_sub_user_mapping.user, -12, -1)}_${substr(local.app_sub_user_mapping.subscription_id, -12, -1)}" => entry }
   scope                = "/subscriptions/${each.value.subscription_id}"
   role_definition_name = "User Access Administrator"
   principal_id         = each.value.user
 }
-
-
-# resource "aws_s3_bucket_object" "object" {
-#   for_each       = var.enable_aad_permissions[1] ? tomap({for entry in local.app_sub_user_mapping : "${substr(entry.user, -12, -1)}_${substr(entry.subscription_id, -12, -1)}" => entry }) : {}
-#   for_each       = { for entry in local.app_sub_user_mapping : "${substr(entry.user, -12, -1)}_${substr(entry.subscription_id, -12, -1)}" => entry }
-
-# count                 = var.enable_sub_logs ? 1 : 0
-
-#   bucket         = each.value.bucket_backup
-#   key            = format("%s/", each.value.folder)
-#   depends_on     = [aws_s3_bucket.bucket_backup]
-#   force_destroy  = true
-# } 
