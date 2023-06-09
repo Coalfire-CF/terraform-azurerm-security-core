dca456860a0e97e23d522cef39c690131642f96f
Douglas Francis <douglas@poshops.io>
2023-06-09 10:56:35 -0500

    added private dns zone logic

--- locals.tf
+++ locals.tf
@@ -1,6 +1,5 @@
 locals {
   storage_name_prefix = replace(var.resource_prefix, "-", "")
-  enable_private_dns  = var.private_dns_zone_name != null ? true : false
   app_sub_user_mapping = distinct(flatten([for user in var.admin_principal_ids : [
     for app, sub in var.app_subscription_ids : {
       user            = user
@@ -11,4 +10,17 @@ locals {
     Function = "Networking"
     Plane    = "Core"
   })
+
+  # DNS
+  regional_private_dns_zones = {
+    azure_kubernetes_service_management = [
+      for location in [var.location, var.dr_location] :
+      "privatelink.${location}.cx.aks.containerservice.azure.us"
+    ]
+    azure_kusto = [
+      for location in [var.location, var.dr_location] :
+      "privatelink.${location}.kusto.usgovcloudapi.net"
+    ]
+  }
+  private_dns_zones = distinct(concat(var.azure_private_dns_zones, var.custom_private_dns_zones, flatten(values(local.regional_private_dns_zones))))
 }
