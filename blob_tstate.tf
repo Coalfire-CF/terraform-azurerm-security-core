0c207f3a2ced8289d7111eb0154b5d12e45ed182
Douglas Francis <douglas@poshops.io>
2023-06-14 10:13:52 -0400

    updated state storage account to GRS"

--- blob_tstate.tf
+++ blob_tstate.tf
@@ -4,7 +4,7 @@ resource "azurerm_storage_account" "tf_state" {
   resource_group_name             = azurerm_resource_group.core.name
   location                        = var.location
   account_tier                    = "Standard"
-  account_replication_type        = "LRS"
+  account_replication_type        = "GRS"
   min_tls_version                 = "TLS1_2"
   enable_https_traffic_only       = true
   allow_nested_items_to_be_public = false
