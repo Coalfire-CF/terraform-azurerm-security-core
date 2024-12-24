9b237551ed0bb1a9e9b657c16303c05f987aea61
Herman Wong <herman.wong@coalfire.com>
2024-12-24 10:57:39 -0800

    Fix versions.

--- required_version.tf
+++ required_version.tf
@@ -1,7 +1,14 @@
 terraform {
   required_version = "~>1.0"
-  azurerm = {
+  required_providers {
+    azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 3.45.0"
+      #version = "~> 4.0" # 4.0 isn't compatible with pak currently
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0" # 3.0 appears bugged: https://github.com/hashicorp/terraform-provider-azuread/issues/1526
+    }
+  }
 }
