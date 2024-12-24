4b45af4c43abfdee551eee8a62121ef58a601b22
Herman Wong <herman.wong@coalfire.com>
2024-12-24 14:08:12 -0800

    Update provider version.

--- required_version.tf
+++ required_version.tf
@@ -3,8 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      #version = "~> 3.45.0"
-      version = "~> 4.0" # 4.0 isn't compatible with pak currently
+      version = "~> 4.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
